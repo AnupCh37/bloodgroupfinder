3299bf8dc7e8365c3c00e64698b18378d5a5386f
AnupCh37 <anup37@icloud.com>
2026-01-24 23:12:52 +0545

    added logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+function Navbar() {
+    return (
+<>
+<nav className='flex block'>
+<img className='h-[50px] w-[200px] m-3' src={logo} alt="test"  />
+
+   </nav>
+</>
+
+    );
+}
+export default Navbar;
