99b5669499e64f63806f3ce44bc41bb31d1e60cb
AnupCh37 <anup37@icloud.com>
2026-01-27 21:57:30 +0545

    some font added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import './components.css'
 function Navbar() {
     return (
-<>
-<nav className='flex '>
-<img className='h-[50px] w-[200px] m-3' src={logo} alt="logo"  />
-<div className='flex  space-x-5 relative ml-auto'>
-  <Link to="/">Home</Link>
-  <Link to="/find-donor">Find Donor</Link>
-  <Link to="/register-donor">Register Donor</Link>
-  <Link to="/profile">Profile</Link>
-  <Link to="/login">Login</Link>
-</div>
-   </nav>
-</>
+        <>
+            <nav className='flex '>
+                <img className='h-[50px] w-[200px] m-3' src={logo} alt="logo" />
+                <div className='nav-div '>
+                    <Link to="/">Home</Link>
+                    <Link to="/find-donor">Find Donor</Link>
+                    <Link to="/register-donor">Register Donor</Link>
+                    <Link to="/profile">Profile</Link>
+                    <Link to="/login">Login</Link>
+                </div>
+            </nav>
+        </>
     );
 }
 export default Navbar;
