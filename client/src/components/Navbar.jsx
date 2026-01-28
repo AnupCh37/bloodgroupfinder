b58dc268b90f57a574afbe7e405a51d7dc23a613
AnupCh37 <anup37@icloud.com>
2026-01-28 08:29:44 +0545

    navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,11 +8,11 @@ function Navbar() {
             <nav className='flex '>
                 <img className='h-[50px] w-[200px] m-3' src={logo} alt="logo" />
                 <div className='nav-div '>
-                    <Link to="/">Home</Link>
-                    <Link to="/find-donor">Find Donor</Link>
-                    <Link to="/register-donor">Register Donor</Link>
-                    <Link to="/profile">Profile</Link>
-                    <Link to="/login">Login</Link>
+                    <Link className='nav-link' to="/">Home</Link>
+                    <Link className='nav-link'  to="/find-donor">Find Donor</Link>
+                    <Link className='nav-link' to="/register-donor">Register Donor</Link>
+                    <Link className='nav-link' to="/profile">Profile</Link>
+                    <Link className='nav-link' to="/login">Login</Link>
                 </div>
             </nav>
         </>
